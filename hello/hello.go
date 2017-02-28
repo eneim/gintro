6f30713c1dc5b8831ce0c466505232aca233864f
Bassirou Sarr <elhadjibassirou.sarr@g.softbank.co.jp>
2017-03-01 01:50:06 +0900

    hello

---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var name string
+	if len(os.Args) >= 2 {
+		name = os.Args[1]
+	} else {
+		name = "World"
+	}
+	fmt.Printf("Hello, %s!\n", name)
+}
