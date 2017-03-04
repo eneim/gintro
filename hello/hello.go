9f7b46102aedc56afa5efc6dd49040a4831ef906
bassirou-sarr <tsub1208>
2017-03-05 00:32:26 +0900

    added more explanations

--- hello/hello.go
+++ hello/hello.go
@@ -10,15 +10,35 @@ package main
 //	import "packageA"
 //	import "packageB"
 import (
+	// fmt implements formatted I/O with functions analogous to C's printf and scanf
+	// Try to be familiar with this package as you'll definitely use it frequently
 	"fmt"
+
+	// os provides a platform-independent interface to operating system functionality
 	"os"
 )
 
 // In this example, our package is the main package and must have a main() function.
 // The main() function takes no argument and returns nothing.
 func main() {
+	// Use the var keyword to declare a variable.
+	// A newly declared varialbe is assigne the zero value of the type it represents.
+	// In this case, name has the string's zero value "".
+	// Inside a function, we can implicitly declare the type of a variable
+	// with the := operator.
+	//		name := ""
+	// Since "" is the value of a string, go will implicitly declare name as type string
+	// Another example: Suppose we have the following function:
+	//		func getString() string {
+	// 			return "I am a string!"
+	//		}
+	// You could use the := operator to implicitly declare a variable of type string
+	// and assign the returned value of the function, provided you do it inside a function
+	//		str := getString()
 	var name string
 	if len(os.Args) >= 2 {
+		// os.Args is a slice of strings. you can do this assignment only if name was
+		// previously declared as type string. go is a statically typed language!!!
 		name = os.Args[1]
 	} else {
 		name = "World"
